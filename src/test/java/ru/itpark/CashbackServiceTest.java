1eac97aa7798dfa56e2b731bdd935352e868206b
stlkralexdemo <stlkralex@yandex.ru>
2018-12-07 23:10:42 +0300

    исправление бага

--- src/test/java/ru/itpark/CashbackServiceTest.java
+++ src/test/java/ru/itpark/CashbackServiceTest.java
@@ -9,13 +9,13 @@ class CashbackServiceTest {
     @Test
     void calculateCashback() {
         CashbackService cashbackService = new CashbackService();
-        int cashback = cashbackService.CalculateCashback(4_000, 7_000, 23_753);
+        int cashback = cashbackService.calculateCashback(4_000, 7_000, 23_753);
         assertEquals(892, cashback);
     }
 
     {
         CashbackService cashbackService = new CashbackService();
-        int cashback = cashbackService.CalculateCashback(4_000, 700_000, 23_753);
+        int cashback = cashbackService.calculateCashback(4_000, 700_000, 23_753);
         assertEquals(3000, cashback);
     }
 }
